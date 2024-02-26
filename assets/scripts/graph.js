1ece97a2d41f94b480d4e13b3516d007801aa874
lastlegume <lastlegume@gmail.com>
2024-02-25 19:05:57 -0600

    created graphing api and wrote population growth post

---
+++ assets/scripts/graph.js
@@ -0,0 +1,351 @@
+//give the method the function and it'll graph it
+function graph(func, canvas, xInc, xlab, ylab, xlim, ylim, col) {
+    let padding = 40;
+
+    if (!xlim) {
+        xlim = [0, 100];
+    } else {
+        xInc = (xlim[1] - xlim[0]) / (canvas.width - padding);
+    }
+    if (!ylim) {
+        ylim = [0, 100];
+    }
+
+    ctx = canvas.getContext('2d');
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.lineWidth = 1;
+    ctx.textAlign = "center";
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "9px";
+    //labels
+    if (xlab) {
+        ctx.fillText(xlab, canvas.width / 2 + padding / 2, canvas.height);
+    }
+    if (ylab) {
+        ctx.rotate(-Math.PI / 2);
+        ctx.fillText(ylab, (canvas.height - padding) * -.5, 9);
+        ctx.rotate(Math.PI / 2);
+    }
+    //grid
+
+    // x=a lines
+    let xGridInc = Math.round(canvas.width / 30);
+    let xGridSkips = Math.ceil(24 / xGridInc);
+    for (let i = 0; i <= canvas.width; i += xGridInc) {
+        ctx.globalAlpha = 0.1;
+        ctx.beginPath();
+        ctx.moveTo(padding + i, 0);
+        ctx.lineTo(padding + i, canvas.height - padding);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.globalAlpha = 1;
+        if (i % (xGridInc * xGridSkips) == 0&&i+xGridInc+padding<=canvas.width) {
+            let label = (i * xInc) + xlim[0];
+            ctx.fillText((label == 0) ? "0.00" : (Math.abs(label) < 10000 && Math.abs(label) >= .01) ? label.toPrecision(3) : label.toExponential(2), padding + i, (canvas.height - padding) + padding / 2, 24);
+        }
+    }
+    // y=a lines
+    let yGridInc = Math.max(Math.round(canvas.width / 30), 15);
+    for (let i = canvas.height - padding; i >= 0; i -= yGridInc) {
+        ctx.globalAlpha = 0.1;
+        ctx.beginPath();
+        ctx.moveTo(padding, i);
+        ctx.lineTo(canvas.width, i);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.globalAlpha = 1;
+        let label = (1 - (i / (canvas.height - padding))) * (ylim[1] - ylim[0]) + ylim[0];
+        ctx.fillText((label == 0) ? "0.00" : (Math.abs(label) < 10000 && Math.abs(label) >= .01) ? label.toPrecision(3) : label.toExponential(2), padding - 13, i, 22)
+    }
+    //graph 
+    ctx.globalAlpha = 1;
+
+    ctx.beginPath();
+    if(col.includes("#")||col.includes("rgb"))
+        ctx.strokeStyle = col;
+    else
+        ctx.strokeStyle = "#00FFA0";
+    ctx.moveTo(padding, canvas.height - padding);
+    let lastOvershoot = -2;
+    let overshoot = -1;
+    let x = xlim[0];
+    for (let i = 0; i < canvas.width - padding; i++) {
+        let pctY = (solve(func.replaceAll('x', x)) - ylim[0]) / (ylim[1] - ylim[0]);
+        overshoot = -1;
+        for (let s = 0; s < 1; s += .1) {
+            pctY = (solve(func.replaceAll('x', x + s * xInc)) - ylim[0]) / (ylim[1] - ylim[0]);
+            if (pctY >= 1) {
+                if (overshoot == -1 && lastOvershoot == -1) {
+                    overshoot = 0;
+                    ctx.lineTo(padding + i, 0);
+                }
+                else
+                    ctx.moveTo(padding + i, 0);
+                lastOvershoot = 0;
+            } else if (pctY <= 0) {
+                if (overshoot == -1 && lastOvershoot == -1) {
+                    overshoot = 1;
+                    ctx.lineTo(padding + i, (canvas.height - padding));
+                }
+                else
+                    ctx.moveTo(padding + i, (canvas.height - padding));
+                lastOvershoot = 1;
+
+            }
+        }
+        if(pctY>=0&&pctY<=1)
+            lastOvershoot = -1;
+        ctx.lineTo(padding + i, (canvas.height - padding) * (1 - Math.max(0, Math.min(1, pctY))));
+
+
+        x += xInc;
+
+    }
+    ctx.stroke();
+    ctx.closePath();
+    //axes
+    ctx.lineWidth = 2;
+
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.beginPath();
+    ctx.moveTo(padding, canvas.height - padding);
+    ctx.lineTo(canvas.width, canvas.height - padding);
+    ctx.moveTo(padding, canvas.height - padding);
+    ctx.stroke();
+    ctx.lineTo(padding, 0);
+    ctx.stroke();
+    ctx.closePath();
+
+}
+//give the method a list of y coords and it'll graph it
+function graphList(yVals, canvas, xInc, xlab, ylab, xlim, ylim, col) {
+    let padding = 40;
+
+    if (!xlim) {
+        xlim = [0, 100];
+    } else {
+        xInc = (xlim[1] - xlim[0]) / (canvas.width - padding);
+    }
+    if (!ylim) {
+        ylim = [0, 100];
+    }
+
+    ctx = canvas.getContext('2d');
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.lineWidth = 1;
+    ctx.textAlign = "center";
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "9px";
+    //labels
+    if (xlab) {
+        ctx.fillText(xlab, canvas.width / 2 + padding / 2, canvas.height);
+    }
+    if (ylab) {
+        ctx.rotate(-Math.PI / 2);
+        ctx.fillText(ylab, (canvas.height - padding) * -.5, 9);
+        ctx.rotate(Math.PI / 2);
+    }
+    //grid
+
+    // x=a lines
+    let xGridInc = Math.round(canvas.width / 30);
+    let xGridSkips = Math.ceil(24 / xGridInc);
+    for (let i = 0; i <= canvas.width; i += xGridInc) {
+        ctx.globalAlpha = 0.1;
+        ctx.beginPath();
+        ctx.moveTo(padding + i, 0);
+        ctx.lineTo(padding + i, canvas.height - padding);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.globalAlpha = 1;
+        if (i % (xGridInc * xGridSkips) == 0&&i+xGridInc+padding<=canvas.width) {
+            let label = (i * xInc) + xlim[0];
+            ctx.fillText((label == 0) ? "0.00" : (Math.abs(label) < 10000 && Math.abs(label) >= .01) ? label.toPrecision(3) : label.toExponential(2), padding + i, (canvas.height - padding) + padding / 2, 24);
+        }
+    }
+    // y=a lines
+    let yGridInc = Math.max(Math.round(canvas.width / 30), 15);
+    for (let i = canvas.height - padding; i >= 0; i -= yGridInc) {
+        ctx.globalAlpha = 0.1;
+        ctx.beginPath();
+        ctx.moveTo(padding, i);
+        ctx.lineTo(canvas.width, i);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.globalAlpha = 1;
+        let label = (1 - (i / (canvas.height - padding))) * (ylim[1] - ylim[0]) + ylim[0];
+        ctx.fillText((label == 0) ? "0.00" : (Math.abs(label) < 10000 && Math.abs(label) >= .01) ? label.toPrecision(3) : label.toExponential(2), padding - 13, i, 22)
+    }
+    //graph 
+    ctx.globalAlpha = 1;
+
+    ctx.beginPath();
+    if(col.includes("#")||col.includes("rgb"))
+        ctx.strokeStyle = col;
+    else
+        ctx.strokeStyle = "#00FFA0";
+    ctx.moveTo(padding, canvas.height - padding);
+    let lastOvershoot = -2;
+    let overshoot = -1;
+    let x = xlim[0];
+    for (let i = 0; i < canvas.width - padding; i++) {
+        let pctY = (yVals[i] - ylim[0]) / (ylim[1] - ylim[0]);
+        overshoot = -1;
+        for (let s = 0; s < 1; s += .01) {
+            pctY = (yVals[i] - ylim[0]) / (ylim[1] - ylim[0]);
+            if (pctY >= 1) {
+                if (overshoot == -1 && lastOvershoot == -1) {
+                    overshoot = 0;
+                    ctx.lineTo(padding + i, 0);
+                }
+                else
+                    ctx.moveTo(padding + i, 0);
+                lastOvershoot = 0;
+            } else if (pctY <= 0) {
+                if (overshoot == -1 && lastOvershoot == -1) {
+                    overshoot = 1;
+                    ctx.lineTo(padding + i, (canvas.height - padding));
+                }
+                else
+                    ctx.moveTo(padding + i, (canvas.height - padding));
+                lastOvershoot = 1;
+
+            }
+        }
+        if(pctY>=0&&pctY<=1)
+            lastOvershoot = -1;
+        ctx.lineTo(padding + i, (canvas.height - padding) * (1 - Math.max(0, Math.min(1, pctY))));
+
+
+        x += xInc;
+
+    }
+    ctx.stroke();
+    ctx.closePath();
+    //axes
+    ctx.lineWidth = 2;
+
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.beginPath();
+    ctx.moveTo(padding, canvas.height - padding);
+    ctx.lineTo(canvas.width, canvas.height - padding);
+    ctx.moveTo(padding, canvas.height - padding);
+    ctx.stroke();
+    ctx.lineTo(padding, 0);
+    ctx.stroke();
+    ctx.closePath();
+
+}
+
+
+
+// test data set for the equation solver
+// console.log(solve("2 ^ ( 3 ! * ( 2 * 1 ) ! ) + ( 1 - 3 )"));
+// console.log(solve("ln 5 * ln 6 + ( 6 ! * .5 + 3 )"));
+// console.log(solve("e ^ ( 1 + 2 ) + pi"));
+// console.log(solve("tan ( pi / 2 )"));
+function solve(eq) {
+    let terms = eq.split(" ");
+    let start = 0;
+    let sol = 0;
+    //resolves everything in parentheses
+    for (let i = 0; i < terms.length; i++) {
+        if (terms[i] === "e")
+            terms[i] = Math.E;
+        if (terms[i] === "pi")
+            terms[i] = Math.PI;
+        if (terms[i] === "(") {
+            start = i;
+            let numParenthesis = 1;
+            while (numParenthesis > 0) {
+                i++;
+                if (terms[i] === "(")
+                    numParenthesis++;
+                else if (terms[i] === ")")
+                    numParenthesis--;
+                else if (terms[i] === "e")
+                    terms[i] = Math.E;
+                else if (terms[i] === "pi")
+                    terms[i] = Math.PI;
+            }
+            sol = solve(terms.slice(start + 1, i).join(" "));
+            terms.splice(start, i - start + 1, sol);
+            i = start;
+        }
+    }
+    for (let i = 0; i < terms.length; i++) {
+        if (terms[i] === "^") {
+            sol = (terms[i - 1] * 1) ** (terms[i + 1] * 1);
+            terms.splice(i - 1, 3, sol);
+            i--;
+        } else if (terms[i] === "!") {
+            sol = factorial(terms[i - 1] * 1);
+            terms.splice(i - 1, 2, sol);
+            i--;
+        } else if (terms[i] === "ln") {
+            sol = Math.log(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        } else if (terms[i] === "log") {
+            sol = Math.log(terms[i + 2] * 1) / Math.log(terms[i + 1] * 1);
+            terms.splice(i, 3, sol);
+        } else if (terms[i] === "sin") {
+            sol = Math.sin(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        } else if (terms[i] === "cos") {
+            sol = Math.cos(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        } else if (terms[i] === "tan") {
+            sol = Math.tan(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        } else if (terms[i] === "csc") {
+            sol = 1 / Math.sin(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        } else if (terms[i] === "sec") {
+            sol = 1 / Math.cos(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        } else if (terms[i] === "cot") {
+            sol = 1 / Math.tan(terms[i + 1] * 1);
+            terms.splice(i, 2, sol);
+        }
+    }
+    for (let i = 0; i < terms.length; i++) {
+
+        if (terms[i] === "*") {
+            sol = (terms[i - 1] * 1) * (terms[i + 1] * 1);
+            terms.splice(i - 1, 3, sol);
+            i--;
+        } else if (terms[i] === "/") {
+            sol = (terms[i - 1] * 1) / (terms[i + 1] * 1);
+            terms.splice(i - 1, 3, sol);
+            i--;
+        }
+    }
+    for (let i = 0; i < terms.length; i++) {
+        if (terms[i] === "+") {
+            sol = terms[i - 1] * 1 + terms[i + 1] * 1;
+            terms.splice(i - 1, 3, sol);
+            i--;
+        } else if (terms[i] === "-") {
+            sol = terms[i - 1] * 1 - terms[i + 1] * 1;
+            terms.splice(i - 1, 3, sol);
+            i--;
+        }
+
+        // if (i + 1 >= terms.length && terms.length > 1 && counter < 50) {
+        //     i = 0;
+        //     counter++;
+        //     noexponents = !terms.includes("^") && !terms.includes("!") && !terms.includes("log") && !terms.includes("ln");;
+        //     nomult = !terms.includes("/") && !terms.includes("*");
+
+        // }
+    }
+    return terms[0];
+}
+
+function factorial(n) {
+    if (n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
---
+++ assets/scripts/growthrates.js
@@ -0,0 +1,71 @@
+const expRSlider = document.getElementById("exponential-r");
+const expRValue = document.getElementById("exponential-r-value");
+const expN0Slider = document.getElementById("exponential-n0");
+const expN0Value = document.getElementById("exponential-n0-value");
+const expGraph = document.getElementById("basicExponentialModel");
+expGraph.width = Math.min(window.innerWidth*.9, 610);
+expRSlider.addEventListener('input', () => updateExpGraph());
+expN0Slider.addEventListener('input', () => updateExpGraph());
+
+
+const logisticRSlider = document.getElementById("logistic-r");
+const logisticRValue = document.getElementById("logistic-r-value");
+const logisticN0Slider = document.getElementById("logistic-n0");
+const logisticN0Value = document.getElementById("logistic-n0-value");
+const logisticKSlider = document.getElementById("logistic-K");
+const logisticKValue = document.getElementById("logistic-K-value");
+const logisticGraph = document.getElementById("logisticModel");
+const logisticGraphdNdt = document.getElementById("logisticModeldNdt");
+const logisticGraphdNdtN = document.getElementById("logisticModeldNdtN");
+
+logisticGraph.width = Math.min(window.innerWidth*.9, 610);
+logisticGraphdNdt.width = logisticGraph.width/2-20;
+logisticGraphdNdtN.width = logisticGraph.width/2-20;
+
+logisticRSlider.addEventListener('input', () => updateLogisticGraph());
+logisticN0Slider.addEventListener('input', () => updateLogisticGraph());
+logisticKSlider.addEventListener('input', () => updateLogisticGraph());
+
+
+updateExpGraph();
+updateLogisticGraph();
+function updateExpGraph(){
+    expRValue.textContent = "r value: "+ expRSlider.value;
+    expN0Value.innerHTML = "<math><msub><mi>N</mi><mn>0</mn></msub></math> value: "+ expN0Slider.value;
+
+    let func = "N0 * e ^ ( r * x )";
+    graph(func.replaceAll('r', expRSlider.value).replaceAll('N0', expN0Slider.value), expGraph, .005, "Time", "Population", [0, 7], [0, 100], "#00FFA0");
+}
+function updateLogisticGraph(){
+    logisticRValue.textContent = "r value: "+ logisticRSlider.value;
+    logisticN0Value.innerHTML = "<math><msub><mi>N</mi><mn>0</mn></msub></math> value: "+ logisticN0Slider.value;
+    logisticKValue.textContent = "K value: "+ logisticKSlider.value;
+
+    let func = "N0 * ( K / ( ( K - N0 ) * e ^ ( -1 * r * x ) + N0 ) )".replaceAll('r', logisticRSlider.value).replaceAll('N0', logisticN0Slider.value).replaceAll('K', logisticKSlider.value);
+    let diffFunc = "r * N * ( 1 - ( N / K ) )".replaceAll('r', logisticRSlider.value).replaceAll('K', logisticKSlider.value);
+
+    let xlim = [0,50]
+    let xInc = (xlim[1] - xlim[0]) / (logisticGraph.width - 40);
+    let x = xlim[0];
+    let nVals = [];
+    let dNdtVals = [];
+    let dNdtNVals = [];
+    for(let i = 0;i<logisticGraph.width - 40;i++){
+        nVals.push(solve(func.replaceAll('x',x)));
+        x+=xInc;
+    }
+    let nlim = [logisticN0Slider.value*1, logisticKSlider.value*1].sort();
+    let nInc = (nlim[1] - nlim[0]) / (logisticGraphdNdt.width - 40);
+    let n = nlim[0];
+    for(let i = 0;i<logisticGraphdNdt.width - 40;i++){
+        let dNdt = solve(diffFunc.replaceAll('N',n));
+        dNdtVals.push(dNdt);
+        dNdtNVals.push(dNdt/n);
+        n+=nInc;
+    }
+
+    graphList(nVals, logisticGraph, .005, "Time", "Population Size", xlim, [0, 1000], "#009FFF");
+    graphList(dNdtVals, logisticGraphdNdt, .005, "Population Size", "Population Growth Rate", nlim, [Math.min.apply(null, dNdtVals), Math.max.apply(null, dNdtVals)], "#FF9FFF");
+    graphList(dNdtNVals, logisticGraphdNdtN, .005, "Population Size", "Population Growth Rate Per Capita", nlim, [Math.min.apply(null, dNdtNVals), Math.max.apply(null, dNdtNVals)], "#55E0F0");
+
+}
