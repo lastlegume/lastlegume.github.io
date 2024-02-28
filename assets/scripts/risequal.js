1749fd2baf76c42e9900a673ea14ee55a01282ae
lastlegume <lastlegume@gmail.com>
2024-02-28 16:37:52 -0600

    added addendum to population growth post

---
+++ assets/scripts/risequal.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('graph');
+const rInput = document.getElementById('r');
+const colorInput = document.getElementById('color');
+const xminInput = document.getElementById('minx');
+const xmaxInput = document.getElementById('maxx');
+canvas.width = Math.min(window.innerWidth*.9,1000);
+const yminInput = document.getElementById('miny');
+const ymaxInput = document.getElementById('maxy');
+
+rInput.addEventListener('input', () => updateGraph());
+colorInput.addEventListener('input', () => updateGraph());
+
+xminInput.addEventListener('input', () => updateGraph());
+xmaxInput.addEventListener('input', () => updateGraph());
+
+yminInput.addEventListener('input', () => updateGraph());
+ymaxInput.addEventListener('input', () => updateGraph());
+
+updateGraph();
+function updateGraph() {
+    let func = "e ^ ( r * x ) - ( 1 + r ) ^ x".replaceAll("r", rInput.value);
+    let xlim = [xminInput.value * 1, xmaxInput.value * 1].sort((a, b) => a - b);
+    let ylim = [yminInput.value * 1, ymaxInput.value * 1].sort((a, b) => a - b);
+    if (xlim[0] == xlim[1])
+        xlim[1]++;
+    if (ylim[0] == ylim[1])
+        ylim[1]++;
+    
+    graph(func, canvas, .01, "x", "y", xlim, ylim, colorInput.value)
+}
