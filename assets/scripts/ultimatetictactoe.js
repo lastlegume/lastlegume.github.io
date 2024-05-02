28a098cc964f02d200ec79837d5b99dc8aaffcfc
lastlegume <lastlegume@gmail.com>
2024-05-01 21:32:25 -0500

    bug fix

--- assets/scripts/ultimatetictactoe.js
+++ assets/scripts/ultimatetictactoe.js
@@ -53,7 +53,7 @@ function drawBoard() {
     ctx.strokeStyle = "#EEEEEE";
     for (let r = 0; r < 3; r++) {
         for (let c = 0; c < 3; c++) {
-            if (currentBoard == (r * 3 + c)||(currentBoard==-1&&overallBoard[r*3+c]==0)) {
+            if (currentBoard == (r * 3 + c) || (currentBoard == -1 && overallBoard[r * 3 + c] == 0)) {
                 ctx.fillStyle = "rgba(81,232,133,.4)";
                 ctx.fillRect(size / 3 * c + 1, size / 3 * r + 1, size / 3 - 2, size / 3 - 2);
 
@@ -74,23 +74,23 @@ function drawBoard() {
         for (let c = 0; c < 3; c++) {
             ctx.fillStyle = "#FFFFFF";
             ctx.font = (size / 3) + "px Arial";
-            ctx.fillText(overallBoard[r*3+c] == 1 ? "X" : overallBoard[r*3+c] == 2 ? "O" : "", size / 3 * c + 16, size / 3 * (r + 1) - 16, size / 3 - 32);
+            ctx.fillText(overallBoard[r * 3 + c] == 1 ? "X" : overallBoard[r * 3 + c] == 2 ? "O" : "", size / 3 * c + 16, size / 3 * (r + 1) - 16, size / 3 - 32);
         }
     }
     ctx.lineWidth = 3;
     ctx.strokeStyle = "#FFFFFF";
     drawGrid(ctx, 0, 0, size, size);
-    if(turn<0){
+    if (turn < 0) {
         ctx.fillStyle = "rgb(81,232,133)";
-            ctx.font = (size / 9) + "px Arial";
-            ctx.fillText("Click", size/24, size*3/5, size/3-size/12);
-            ctx.fillText("To", 9*size/24, size*3/5, size/3-size/12);
-            ctx.fillText("Start", 17*size/24, size*3/5, size/3-size/12);
+        ctx.font = (size / 9) + "px Arial";
+        ctx.fillText("Click", size / 24, size * 3 / 5, size / 3 - size / 12);
+        ctx.fillText("To", 9 * size / 24, size * 3 / 5, size / 3 - size / 12);
+        ctx.fillText("Start", 17 * size / 24, size * 3 / 5, size / 3 - size / 12);
 
     }
 }
 function runTurn(e) {
-    if(turn==1001){
+    if (turn == 1001) {
         reset();
         currentBoard = -1;
         lastMove = Array(2).fill(-1);
@@ -103,9 +103,9 @@ function runTurn(e) {
 
     coords[0] = Math.floor(x * 9 / size);
     coords[1] = Math.floor(y * 9 / size);
-    if(turn==-1){
+    if (turn == -1) {
         turn++;
-        gamemode = Math.floor(coords[0]/3);
+        gamemode = Math.floor(coords[0] / 3);
         currentBoard = -1;
         drawBoard();
         text.innerHTML = "<b>Current Turn: </b> X";
@@ -119,28 +119,31 @@ function runTurn(e) {
         return;
     game[coords[1]][coords[0]] = turn % 2 + 1;
     currentBoard = (coords[1] % 3 * 3 + coords[0] % 3);
-    if(overallBoard[currentBoard]>0)
-        currentBoard = -1;
+
     lastMove = coords;
     for (let r = 0; r < 3; r++) {
         for (let c = 0; c < 3; c++) {
-            let tempBoard = game.slice(r*3, r*3+3);
-            for(let i = 0;i<tempBoard.length;i++){
-                tempBoard[i] = tempBoard[i].slice(c*3, c*3+3) ;
+            let tempBoard = game.slice(r * 3, r * 3 + 3);
+            for (let i = 0; i < tempBoard.length; i++) {
+                tempBoard[i] = tempBoard[i].slice(c * 3, c * 3 + 3);
             }
-            overallBoard[(r*3+c)] = getWinner(tempBoard);
+            overallBoard[(r * 3 + c)] = getWinner(tempBoard);
+            // if(overallBoard[(r*3+c)]>0&&r*3+c==currentBoard)
+            //     currentBoard=-1;
         }
     }
-    let winner = getWinner([overallBoard.slice(0,3), overallBoard.slice(3,6), overallBoard.slice(6,9)]);
-    if(winner>0){
+    if (overallBoard[currentBoard] > 0)
+        currentBoard = -1;
+    let winner = getWinner([overallBoard.slice(0, 3), overallBoard.slice(3, 6), overallBoard.slice(6, 9)]);
+    if (winner > 0) {
         //winning code goes here
         console.log(`${winner} is the winner`);
-        text.innerHTML = "<b>"+((winner==1)?"X":"O")+"</b> is the winner";
+        text.innerHTML = "<b>" + ((winner == 1) ? "X" : "O") + "</b> is the winner";
         currentBoard = -2;
         lastMove = Array(2).fill(-1);
         turn = 1000;
-    }else
-        text.innerHTML = "<b>Current Turn: </b> "+((turn%2==1)?"X":"O");
+    } else
+        text.innerHTML = "<b>Current Turn: </b> " + ((turn % 2 == 1) ? "X" : "O");
 
     //console.log(overallBoard);
     drawBoard();
@@ -154,9 +157,9 @@ function getWinner(board) {
         if (board[0][i] > 0 && board[0][i] == board[1][i] && board[0][i] == board[2][i])
             return board[0][i];
     }
-    if(board[0][0]>0&&board[0][0]==board[1][1]&&board[0][0]==board[2][2])
+    if (board[0][0] > 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2])
         return board[0][0];
-    if(board[2][0]>0&&board[2][0]==board[1][1]&&board[2][0]==board[0][2])
+    if (board[2][0] > 0 && board[2][0] == board[1][1] && board[2][0] == board[0][2])
         return board[2][0]
     return 0;
 }
