07b2c9d2f14c3505dcb161fbb9ec3e2a3290e100
lastlegume <lastlegume@gmail.com>
2024-08-20 15:22:32 -0500

    fixed total cards issue

--- assets/scripts/deckProbabilities.js
+++ assets/scripts/deckProbabilities.js
@@ -217,5 +217,5 @@ function toggleBasic(){
     }
 }
 function isHeader(str){
-    return /Pok[ée]mon:\s\d+/.test(str)||/Trainer:\s\d+/.test(str)||/Energy:\s\d+/.test(str);
+    return /Pok[ée]mon:\s\d+/.test(str)||/Trainer:\s\d+/.test(str)||/Energy:\s\d+/.test(str)||str.includes("Total Cards:");
 }
