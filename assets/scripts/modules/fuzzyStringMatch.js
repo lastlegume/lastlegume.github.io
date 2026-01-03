7d2375219ef1bdd3bbbff3603071780a1677742d
lastlegume <lastlegume@gmail.com>
2026-01-02 23:25:36 -0600

    switched to Wagner–Fischer for fuzzy matching

--- assets/scripts/modules/fuzzyStringMatch.js
+++ assets/scripts/modules/fuzzyStringMatch.js
@@ -1,4 +1,33 @@
 function fuzzy(guess, answer) {
+    return levenshteinDistance(guess, answer)<4;
+}
+
+//uses the full matrix approach instead of just using two rows
+function levenshteinDistance(guess, answer){
+
+    let mat = Array(guess.length+1).fill().map(()=>Array(answer.length+1));
+    for(let i = 0;i<=guess.length;i++)
+        mat[i][0] = i;
+    for(let i = 1;i<=answer.length;i++)
+        mat[0][i] = i;
+
+    for(let r=1;r<mat.length;r++){
+        for(let c = 1;c<=answer.length;c++){
+            let substitutionCost = (guess.charAt(r-1)==answer.charAt(c-1))?0:1;
+            mat[r][c] = Math.min(mat[r-1][c-1]+substitutionCost, mat[r-1][c]+1, mat[r][c-1]+1);
+        }
+    }
+    
+    return mat[guess.length][answer.length];
+}
+
+
+
+
+
+
+
+function oldFuzzy(guess, answer) {
     guess = guess.toLowerCase().trim();
     answer = answer.toLowerCase().trim();
     // checks if the strings are equals beforehand to skip the iteration if unnecessary
@@ -26,3 +55,5 @@ function fuzzy(guess, answer) {
     //   console.log(score + " vs needed " + neededFuzzyAmount ** fuzziness)
     return score > neededFuzzyAmount ** fuzziness;
 }
+
+
